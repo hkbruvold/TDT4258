9d9a025230b1a50a48dc07cedcb1117cfe79a354
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-02-22 10:00:05 +0100

    Did some bugfix on ex1.c

--- exercise2/ex1.c
+++ exercise2/ex1.c
@@ -7,7 +7,32 @@
 #include "ex1.h"
 
 __int_handler *int_handler(void) {
+  volatile avr32_pio_t *pioc = &AVR32_PIOC;
+  volatile avr32_pio_t *piob = &AVR32_PIOB;
+  
+  struct ledstate ls;
+  
+  if (pioc->pdsr == SW0) {
+    /* rightshift */
+    piob->codr = 0xff;
+    if (ls.leds == 1) {
+      ls.leds = 0x80;
+    } else {
+      ls.leds >> 1;
+    }
+    piob->sodr = ls.leds;
+  }
 
+  if (pioc->pdsr == SW2) {
+    /* leftshift */
+    piob->codr == 0xff;
+    if (ls.leds == 0x80) {
+      ls.leds = 1;
+    } else {
+      ls.leds << 1;
+    }
+    piob->sodr = ls.leds;
+  }
 }
 
 int main(void) {
@@ -17,13 +42,13 @@ int main(void) {
   struct ledstate ls;
 
   /* initialise buttons */
-  pioc.per = BUTTONS;
-  pioc.puer = BUTTONS;
-  pioc.ier = BUTTONS;
+  pioc->per = BUTTONS;
+  pioc->puer = BUTTONS;
+  pioc->ier = BUTTONS;
   
   /* initialise LEDs */
-  piob.per = 0xff;
-  piob.oer = 0xff;
+  piob->per = 0xff;
+  piob->oer = 0xff;
 
   /* initialise interrupt handling */
   set_interrupts_base((void*) AVR32_INTC_ADDRESS);
@@ -33,32 +58,12 @@ int main(void) {
   init_interrupts();
 
   /* turn on first LED */
-  piob.codr = 0xff;
-  ls.leds = 1;
-  piob.sodr = ls.leds;
+  piob->codr = 0xff;
+  ls->leds = 1;
+  piob->sodr = ls.leds;
 
   /* start main loop */
-  while (true) {
-    if (pioc.pdsr == SW0) {
-      /* rightshift */
-      piob.codr = 0xff;
-      if (ls.leds == 1) {
-        ls.leds = 0x80;
-      } else {
-        ls.leds >> 1;
-      }
-      piob.sodr = ls.leds;
-    }
-
-    if (pioc.pdsr == SW2) {
-      /* leftshift */
-      piob.codr == 0xff;
-      if (ls.leds == 0x80) {
-        ls.leds = 1;
-      } else {
-        ls.leds << 1;
-      }
-      piob.sodr = ls.leds;
-    }
+  while (1) {
+    SLEEP(AVR32_PM_SMODE_FROZEN);
   }
 }
