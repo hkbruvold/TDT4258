7995b1aec8a648a8a955f3428f30ae0d69286b82
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-02-23 07:25:54 +0100

    Added basicButtonLED

---
+++ exercise2/basicButtonLED.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *
+ * Exercise 1 ported to C
+ *
+ *****************************************************************************/
+
+#include "basicButtonLED.h"
+
+int main(int argc, char *argv[]) {
+  volatile avr32_pio_t *pioc = &AVR32_PIOC;
+  volatile avr32_pio_t *piob = &AVR32_PIOB;
+
+  /* initialise buttons */
+  pioc->per = 0xff;
+  pioc->puer = 0xff;
+  pioc->ier = 0xff;
+  
+  /* initialise LEDs */
+  piob->per = 0xff;
+  piob->oer = 0xff;
+
+  /* start main loop */
+  while (1) {
+    int a = pioc->pdsr;
+    piob->codr = 0xff;
+    pioc->sodr = a;
+  }
+}
