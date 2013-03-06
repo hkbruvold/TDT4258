3afbc9154e3d956c637577314118cb55ac91efff
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-03-06 21:55:04 +0100

    added a bit more audio init

--- exercise2/ex2.c
+++ exercise2/ex2.c
@@ -8,7 +8,8 @@
 
 volatile avr32_pio_t *piob = &AVR32_PIOB;	// piob; buttons
 volatile avr32_pio_t *pioc = &AVR32_PIOC;	// pioc; leds
-
+volatile avr32_dac_t *dac = &AVR32_ABDAC;
+volatile avr32_pm_t *pm = &AVR32_PM;
 
 int main (int argc, char *argv[]) {
   initHardware();
@@ -31,29 +32,54 @@ void initIntc(void) {
 }
 
 void initButtons(void) {
-  register_interrupt( button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
-  /* (...) */
+  register_interrupt(button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
   piob->per = 0xff;
   piob->puer = 0xff;
   piob->ier = 0xff;
 }
 
 void initLeds(void) {
-  /* (...) */
   pioc->per = 0xff;
   pioc->oer = 0xff;
-  pioc->codr = 0xff;
 }
 
 void initAudio(void) {
-  register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
-  /* (...) */
+  register_interrupt(abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
+  
+  piob->pdr.p20 = 1;
+  piob->pdr.p21 = 1;
+
+  piob->asr.p20 = 1;
+  piob->asr.p21 = 1;
+  
+  pm->pm_gcctrl[6] = data; /* something goes here*/
+  
+  dac->cr.en = 1;
+  dac->ier.tx_ready = 1;
 }
 
 void button_isr(void) {
-  int a = piob->isr;
-  pioc->codr = 0xff;
-  pioc->sodr = a;
+  int pressedButton = piob->isr;
+  pioc->codr = 0xff; // temporary
+  pioc->sodr = pressedButton; // temporary
+  
+  if (pressedButton == SW0) {
+    /* do something */
+  } else if (pressedButton == SW1) {
+    /* do something */
+  } else if (pressedButton == SW2) {
+    /* do something */
+  } else if (pressedButton == SW3) {
+    /* do something */
+  } else if (pressedButton == SW4) {
+    /* do something */
+  } else if (pressedButton == SW5) {
+    /* do something */
+  } else if (pressedButton == SW6) {
+    /* do something */
+  } else if (pressedButton == SW7) {
+    /* do something */
+  }
 }
 
 void abdac_isr(void) {}
--- exercise2/ex2.h
+++ exercise2/ex2.h
@@ -4,30 +4,42 @@
  *
  * Headerfil
  *
+
  *****************************************************************************/
 
-#ifndef OEVING_2_H /* vanlig måte å unngå headerrekursjon på */
-	#define OEVING_2_H
+#ifndef EX_2_H /* vanlig måte å unngå headerrekursjon på */
+#define EX_2_H
 
-	#include <stdio.h>
-	#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	#include <avr32/ap7000.h> /* inkludere Atmel-headerfil */
-	#include <sys/interrupts.h>
+#include <avr32/ap7000.h> /* inkludere Atmel-headerfil */
+#include <sys/interrupts.h>
 
-	/* legg andre "includes" her, sammen med "defines" og prototyper */
-	#define ABDAC_INT_LEVEL 0
-	#define BUTTONS_INT_LEVEL 0
+/* legg andre "includes" her, sammen med "defines" og prototyper */
+#define ABDAC_INT_LEVEL 0
+#define BUTTONS_INT_LEVEL 0
 
-	/* prototyper */
-	int main (int argc, char *argv[]);
-	static void initIntc(void);
-	static void initButtons(void);
-	static void initLeds(void);
-	static void initAudio(void);
-	static void initHardware (void);
+/* Button makros */
+#define SW0 0b1
+#define SW1 0b10
+#define SW2 0b100
+#define SW3 0b1000
+#define SW4 0b10000
+#define SW5 0b100000
+#define SW6 0b1000000
+#define SW7 0b10000000
 
-	static void button_isr(void);
-	static void abdac_isr(void);
+
+/* prototyper */
+int main (int argc, char *argv[]);
+static void initIntc(void);
+static void initButtons(void);
+static void initLeds(void);
+static void initAudio(void);
+static void initHardware (void);
+
+static void button_isr(void);
+static void abdac_isr(void);
 
 #endif
