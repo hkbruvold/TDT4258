15cb5e72b514f70a49f45a4581e06fd7edd7c5c9
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-03-14 18:13:05 +0100

    new file

---
+++ exercise2/notes.c
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * 
+ * Exercise 2 - notes
+ *
+ *****************************************************************************/
+
+#include "notes.h"
+
+
+short getNoteData(char note, int octave, long pos) {
+  int indexFactor = pow(2, 4 - octave);
+  long realPos = pos * indexFactor;
+  short *noteArray;
+
+  switch (note) {
+    case 'c':
+      noteArray = C4;
+      break;
+    case 'C':
+      noteArray = Cs4;
+      break;
+    case 'd':
+      noteArray = D4;
+      break;
+    case 'D':
+      noteArray = Ds4;
+      break;
+    case 'e':
+      noteArray = E4;
+      break;
+    case 'f':
+      noteArray = F4;
+      break;
+    case 'F':
+      noteArray = Fs4;
+      break;
+    case 'g':
+      noteArray = G4;
+      break;
+    case 'G':
+      noteArray = Gs4;
+      break;
+    case 'a':
+      noteArray = A4;
+      break;
+    case 'A':
+      noteArray = As4;
+      break;
+    case 'b':
+      noteArray = B4;
+      break;
+  }
+  
+  return noteArray[realPos % (sizeof(noteArray) / sizeof(noteArray))]
+}
---
+++ exercise2/notes.h
@@ -0,0 +1,16 @@
+/*****************************************************************************
+ *
+ * Exercise 2 - notes
+ *
+ *****************************************************************************/
+
+#ifndef NOTES_H
+#define NOTES_H
+
+#include <math.h>
+
+#include "sinTables.h"
+
+short getNoteData(char note, int octave, long pos); 
+
+#endif
