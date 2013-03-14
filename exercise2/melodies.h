65e742e105e957e1388351b7e8a351b80b12b791
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-03-14 18:14:32 +0100

    Moved from notes.c to melodies, and small fixes

---
+++ exercise2/melodies.h
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * 
+ * Exercise 2 - melodies
+ *
+ *****************************************************************************/
+
+
+#ifndef MELODIES_H
+#define MELODIES_H
+
+extern int GreenSlevesBPM;
+extern int RickRollBPM;
+extern int IsThereAnybodyOutThereBPM;
+extern int BluesLeftBPM;
+extern int BluesRightBPM;
+extern int dunDunDUNNN1BPM;
+extern int dunDunDUNNN2BPM;
+extern int marioBPM;
+
+extern char GreenSleves[];
+extern char RickRoll[];
+extern char IsThereAnybodyOutThere[];
+extern char BluesLeft[];
+extern char BluesRight[];
+extern char dunDunDUNNN1[];
+extern char dunDunDUNNN2[];
+extern char mario[];
+
+
+#endif
