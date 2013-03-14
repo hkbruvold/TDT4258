0623bab2f5b9990550f801888e0905e0e893e83c
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-03-14 18:13:46 +0100

    Added sinTables

---
+++ exercise2/sinTables.h
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ * 
+ * Exercise 2 - sinus tables header file
+ *
+ *****************************************************************************/
+
+#ifndef SINTABLES_H
+#define SINTABLES_H
+
+extern const short C4[184];
+extern const short Cs4[174];
+extern const short D4[164];
+extern const short Ds4[155];
+extern const short E4[146];
+extern const short F4[138];
+extern const short Fs4[130];
+extern const short G4[123];
+extern const short Gs4[116];
+extern const short A4[110];
+extern const short As4[103];
+extern const short B4[98];
+
+
+#endif
