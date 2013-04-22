d93f8c9187cde2387eb7c6123c63b8b5d908eed9
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-04-22 12:02:56 +0200

    some modifications to drivers

--- exercise3/drivers/button.c
+++ exercise3/drivers/button.c
@@ -6,20 +6,80 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/ioport.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+#include "ap7000.h"
+
+/* prototypes */
+
+static int __init button_init(void);
+static void __exit button_exit(void);
+static int driver_open (struct inode *inode, struct file *filp);
+static int driver_release (struct inode *inode, struct file *filp);
+static ssize_t driver_read (struct file *filp, char __user *buff,
+                     size_t count, loff_t *offp);
+static ssize_t driver_write (struct file *filp, const char __user *buff,
+                      size_t count, loff_t *offp);
+
+/* fops-struct */
+
+static struct file_operations driver_fops = {
+  .owner = THIS_MODULE,
+  .read = driver_read,
+  .write = driver_write,
+  .open = driver_open,
+  .release = driver_release
+};
+
+/*****************************************************************************/
+
+
 
 volatile avr32_pio_t *piob = &AVR32_PIOB;
 
-static int __init init_button(void)
+static int __init button_init(void)
 {
+  // allocate device number
+
+  // ask for access to I/O ports
+
+  // initialise PIO hardware
   piob->per = 0xff;
   piob->puer = 0xff;
   piob->ier = 0xff;
+
+  // register device in system
 }
 
-static void __exit exit_button(void)
+static void __exit button_init(void)
 {
 }
 
-module_init(init_button);
-module_exit(exit_button);
+static int driver_open (struct inode *inode, struct file *filp) {
+  return 0;
+}
+
+static int driver_release (struct inode *inode, struct file *filp) {
+  return 0;
+}
+
+static ssize_t driver_read (struct file *filp, char __user *buff,
+              size_t count, loff_t *offp) {
+  return 0;
+}
+
+static ssize_t driver_write (struct file *filp, const char __user *buff,
+               size_t count, loff_t *offp) {
+  return 0;
+}
+
+
+module_init(button_exit);
+module_exit(button_exit);
 
--- exercise3/drivers/led.c
+++ exercise3/drivers/led.c
@@ -6,18 +6,76 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/ioport.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+#include "ap7000.h"
+
+/* prototypes */
+
+static int __init led_init(void);
+static void __exit led_exit(void);
+static int driver_open (struct inode *inode, struct file *filp);
+static int driver_release (struct inode *inode, struct file *filp);
+static ssize_t driver_read (struct file *filp, char __user *buff,
+                     size_t count, loff_t *offp);
+static ssize_t driver_write (struct file *filp, const char __user *buff,
+                      size_t count, loff_t *offp);
+
+/* fops-struct */
+
+static struct file_operations driver_fops = {
+  .owner = THIS_MODULE,
+  .read = driver_read,
+  .write = driver_write,
+  .open = driver_open,
+  .release = driver_release
+};
+
+/*****************************************************************************/
 
 volatile avr32_pio_t *pioc = &AVR32_PIOC;
 
-static int __init init_led(void)
+static int __init led_init(void)
 {
+  // allocate device number
+
+  // ask for access to I/O ports
+
+  // initialise PIO hardware
   pioc->per = 0xff;
-  pioc->our = 0xff;
+  pioc->oer = 0xff;
+
+  // register device in system
 }
 
-static void __exit exit_led(void)
+static void __exit led_init(void)
 {
 }
 
-module_init(init_led);
-module_exit(exit_led);
+static int driver_open (struct inode *inode, struct file *filp) {
+  return 0;
+}
+
+static int driver_release (struct inode *inode, struct file *filp) {
+  return 0;
+}
+
+static ssize_t driver_read (struct file *filp, char __user *buff,
+              size_t count, loff_t *offp) {
+  return 0;
+}
+
+static ssize_t driver_write (struct file *filp, const char __user *buff,
+               size_t count, loff_t *offp) {
+  return 0;
+}
+
+
+module_init(led_init);
+module_exit(led_exit);
