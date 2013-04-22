5a90a10c1ec3ac483f7c8b7cb024ff656b7608a7
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-04-22 12:48:03 +0200

    Added hello world driver

---
+++ exercise3/drivers/hello.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *
+ * Hello World driver
+ *
+ ******************************************************************************/
+
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int __init hello_init(void)
+{
+  printk(KERN_ALERT "Hello World\n");
+  return 0;
+}
+
+static void __exit hello_exit(void)
+{
+  printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
