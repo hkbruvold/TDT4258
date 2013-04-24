397dc2c33f6605e34bc386162e76c45f933037b3
Torkel Berli <torkelbe@dmlab08.idi.ntnu.no>
2013-04-24 12:58:48 +0200

    asdf

--- exercise3/test.c
+++ exercise3/test.c
@@ -8,16 +8,18 @@
 
 int main(void)
 {
-  int button;
+  int button_fd;
   
-  button = open("/dev/button", O_RDONLY);
-  
-  char buffer = 0;
-  
-  if (read(button, buffer, 1) != 0)
+  button_fd = open("/dev/button", O_RDONLY, 0);
+  printf("%i",button_fd);
+
+  char buffer;
+  int e;
+
+  if (e = read(button_fd, buffer, 1) < 0)
   {
-    printf("something went wrong\n");
+    printf("something went wrong, error code %i", e);
   }
-  printf("output: %i", buffer);
-  close(button);
+  printf("output: %i\n", buffer);
+  close(button_fd);
 }
