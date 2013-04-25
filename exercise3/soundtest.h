f910264df4afeb79748f6856568b340024da39c2
Hans-Kristian Bruvold <hanskrib@dmlab05.idi.ntnu.no>
2013-04-25 23:51:41 +0200

    fixed small declaration error

---
+++ exercise3/soundtest.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *
+ * Sound test header file
+ *
+ ******************************************************************************/
+
+#ifndef SOUND_H
+#define SOUND_H
+
+#include <stdio.h>
+#include <math.h>
+#include <linux/soundcard.h>
+#include <fcntl.h>
+
+int main(void); 
+
+#endif
