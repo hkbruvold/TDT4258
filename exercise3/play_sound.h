dbddbf2e78c24d67b9466cd63190c95da516091d
Hans-Kristian Bruvold <hanskrib@dmlab05.idi.ntnu.no>
2013-04-25 23:51:19 +0200

    Added sound player lib files

---
+++ exercise3/play_sound.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *
+ * Sound player header file
+ *
+ ******************************************************************************/
+
+#ifndef PLAYSOUND_H
+#define PLAYSOUND_H
+
+#include <stdio.h>
+#include <math.h>
+#include <linux/soundcard.h>
+#include <fcntl.h> 
+
+playSound(char *filename);
+
+#endif
